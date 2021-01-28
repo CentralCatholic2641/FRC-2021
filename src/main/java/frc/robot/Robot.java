91a589f69e73d3f67f09551fdf9c7926450f2109
CentralCatholic2641 <59667023+CentralCatholic2641@users.noreply.github.com>
2021-01-28 16:00:55 -0500

    BallSearch added using GRIP and camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,19 +3,22 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DrivingSubsystem;
+import frc.robot.vision.BallSearch;
 
 public class Robot extends TimedRobot {
   public static RobotContainer robotContainer;
   public static DrivingSubsystem drivingSubsystem = new DrivingSubsystem();
+  public static BallSearch ballSearcher = new BallSearch();
 
   @Override
   public void robotInit() {
     robotContainer = new RobotContainer();
   }
-
+  
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    System.out.println(ballSearcher.findBlobsOutput().size());
   }
 
   @Override
